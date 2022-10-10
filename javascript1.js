71bb9e35e07551c87d3409467ffd5153edcfb038
AlexF00 <114416846+AlexF00@users.noreply.github.com>
2022-10-10 16:22:19 +0200

    Add files via upload

---
+++ javascript1.js
@@ -0,0 +1,60 @@
+const category = document.querySelectorAll('.containertop2 li')
+
+function show1() {
+  document.querySelector('.dropdownwindow1').style.cssText = 'transform: scaleY(1); opacity:100%; visibility:visible';
+  document.querySelector('main').style.cssText = 'filter: blur(5px) brightness(0.8);'
+  document.querySelector('footer').style.cssText = 'filter: blur(5px) brightness(0.8);'
+}
+function hide1() {
+  document.querySelector('.dropdownwindow1').style.cssText = 'transform: scaleY(0); opacity:0; visibility:hidden';
+  document.querySelector('main').style.cssText = 'filter: blur(0px)'
+  document.querySelector('footer').style.cssText = 'filter: blur(0px)'
+}
+
+
+function show2() {
+  document.querySelector('.dropdownwindow2').style.cssText = 'transform: scaleY(1); opacity:100%; visibility:visible';
+  document.querySelector('main').style.cssText = 'filter: blur(5px) brightness(0.8);'
+  document.querySelector('footer').style.cssText = 'filter: blur(5px) brightness(0.8);'
+}
+function hide2() {
+  document.querySelector('.dropdownwindow2').style.cssText = 'transform: scaleY(0); opacity:0; visibility:hidden';
+  document.querySelector('main').style.cssText = 'filter: blur(0px)'
+  document.querySelector('footer').style.cssText = 'filter: blur(0px)'
+}
+
+
+function show3() {
+  document.querySelector('.dropdownwindow3').style.cssText = 'transform: scaleY(1); opacity:100%; visibility:visible';
+  document.querySelector('main').style.cssText = 'filter: blur(5px) brightness(0.8);'
+  document.querySelector('footer').style.cssText = 'filter: blur(5px) brightness(0.8);'
+}
+function hide3() {
+  document.querySelector('.dropdownwindow3').style.cssText = 'transform: scaleY(0); opacity:0; visibility:hidden';
+  document.querySelector('main').style.cssText = 'filter: blur(0px)'
+  document.querySelector('footer').style.cssText = 'filter: blur(0px)'
+}
+
+
+function show4() {
+  document.querySelector('.dropdownwindow4').style.cssText = 'transform: scaleY(1); opacity:100%; visibility:visible';
+  document.querySelector('main').style.cssText = 'filter: blur(5px) brightness(0.8);'
+  document.querySelector('footer').style.cssText = 'filter: blur(5px) brightness(0.8);'
+}
+function hide4() {
+  document.querySelector('.dropdownwindow4').style.cssText = 'transform: scaleY(0); opacity:0; visibility:hidden';
+  document.querySelector('main').style.cssText = 'filter: blur(0px)'
+  document.querySelector('footer').style.cssText = 'filter: blur(0px)'
+}
+
+
+function show5() {
+  document.querySelector('.dropdownwindow5').style.cssText = 'transform: scaleY(1); opacity:100%; visibility:visible';
+  document.querySelector('main').style.cssText = 'filter: blur(5px) brightness(0.8);'
+  document.querySelector('footer').style.cssText = 'filter: blur(5px) brightness(0.8);'
+}
+function hide5() {
+  document.querySelector('.dropdownwindow5').style.cssText = 'transform: scaleY(0); opacity:0; visibility:hidden';
+  document.querySelector('main').style.cssText = 'filter: blur(0px)'
+  document.querySelector('footer').style.cssText = 'filter: blur(0px)'
+}
