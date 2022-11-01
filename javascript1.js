9b1c57c70bd63e43c1036d2a4b916ea56a86098d
AlexF00 <114416846+AlexF00@users.noreply.github.com>
2022-11-01 18:28:56 +0100

    Add files via upload

--- javascript1.js
+++ javascript1.js
@@ -1,4 +1,6 @@
 'use strict';
+
+//For dropdown window of .containertop2
 function show1() {
   document.querySelector('.dropdownwindow1').style.cssText = 'transform: scaleY(1); opacity:100%; visibility:visible';
   document.querySelector('main').style.cssText = 'filter: blur(5px) brightness(0.8);'
@@ -58,7 +60,7 @@ function hide5() {
   document.querySelector('footer').style.cssText = 'filter: blur(0px)'
 }
 
-
+//For visibility of Account dropdown window
 document.addEventListener('click', function(e){
   if (document.querySelector('.Account').contains(e.target)){
     document.querySelector('.accountwindow').style.cssText = 'transform: scaleY(1); opacity:100%; visibility:visible';
@@ -66,6 +68,8 @@ document.addEventListener('click', function(e){
     document.querySelector('.accountwindow').style.cssText = 'transform: scaleY(0); opacity:0; visibility:hidden';
   }
 });
+
+//For class active and store ids of bikini items
 const wishListStore = []
 let wishListButton = document.getElementsByClassName('wishlistbutton')
 for (let i = 0; i < wishListButton.length; i++) {
@@ -78,6 +82,107 @@ for (let i = 0; i < wishListButton.length; i++) {
     wishListStore.push(this.id)
   }
 })}
+
+//For ActiveSet wishlist store
+function shoppingid1() {
+  let wishlistShoppingItem1 = document.getElementById('bikini01')
+  if (wishlistShoppingItem1.classList.contains('active')) {
+    wishlistShoppingItem1.classList.remove('active')
+    wishListStore.shift(wishlistShoppingItem1.id)
+    document.querySelector('#bikini01 h3').style.cssText = ' '
+  } else {
+    wishlistShoppingItem1.classList.add('active')
+    wishListStore.push(wishlistShoppingItem1.id)
+    document.querySelector('#bikini01 h3').style.cssText = 'background-color: gold; color: black; transition: all 0.3s ease-in-out;'
+  }
+  sessionStorage.setItem('Store', wishListStore)
+}
+
+function shoppingid2() {
+  let wishlistShoppingItem1 = document.getElementById('bikini02')
+  if (wishlistShoppingItem1.classList.contains('active')) {
+    wishlistShoppingItem1.classList.remove('active')
+    wishListStore.shift(wishlistShoppingItem1.id)
+    document.querySelector('#bikini02 h3').style.cssText = ' '
+  } else {
+    wishlistShoppingItem1.classList.add('active')
+    wishListStore.push(wishlistShoppingItem1.id)
+    document.querySelector('#bikini02 h3').style.cssText = 'background-color: gold; color: black; transition: all 0.3s ease-in-out;'
+  }
+  sessionStorage.setItem('Store', wishListStore)
+}
+
+function shoppingid3() {
+  let wishlistShoppingItem1 = document.getElementById('bikini03')
+  if (wishlistShoppingItem1.classList.contains('active')) {
+    wishlistShoppingItem1.classList.remove('active')
+    wishListStore.shift(wishlistShoppingItem1.id)
+    document.querySelector('#bikini03 h3').style.cssText = ' '
+  } else {
+    wishlistShoppingItem1.classList.add('active')
+    wishListStore.push(wishlistShoppingItem1.id)
+    document.querySelector('#bikini03 h3').style.cssText = 'background-color: gold; color: black; transition: all 0.3s ease-in-out;'
+  }
+  sessionStorage.setItem('Store', wishListStore)
+}
+
+function shoppingid4() {
+  let wishlistShoppingItem1 = document.getElementById('bikini04')
+  if (wishlistShoppingItem1.classList.contains('active')) {
+    wishlistShoppingItem1.classList.remove('active')
+    wishListStore.shift(wishlistShoppingItem1.id)
+    document.querySelector('#bikini04 h3').style.cssText = ' '
+  } else {
+    wishlistShoppingItem1.classList.add('active')
+    wishListStore.push(wishlistShoppingItem1.id)
+    document.querySelector('#bikini04 h3').style.cssText = 'background-color: gold; color: black; transition: all 0.3s ease-in-out;'
+  }
+  sessionStorage.setItem('Store', wishListStore)
+}
+
+function shoppingid5() {
+  let wishlistShoppingItem1 = document.getElementById('bikini05')
+  if (wishlistShoppingItem1.classList.contains('active')) {
+    wishlistShoppingItem1.classList.remove('active')
+    wishListStore.shift(wishlistShoppingItem1.id)
+    document.querySelector('#bikini05 h3').style.cssText = ' '
+  } else {
+    wishlistShoppingItem1.classList.add('active')
+    wishListStore.push(wishlistShoppingItem1.id)
+    document.querySelector('#bikini05 h3').style.cssText = 'background-color: gold; color: black; transition: all 0.3s ease-in-out;'
+  }
+  sessionStorage.setItem('Store', wishListStore)
+}
+
+function shoppingid6() {
+  let wishlistShoppingItem1 = document.getElementById('bikini06')
+  if (wishlistShoppingItem1.classList.contains('active')) {
+    wishlistShoppingItem1.classList.remove('active')
+    wishListStore.shift(wishlistShoppingItem1.id)
+    document.querySelector('#bikini06 h3').style.cssText = ' '
+  } else {
+    wishlistShoppingItem1.classList.add('active')
+    wishListStore.push(wishlistShoppingItem1.id)
+    document.querySelector('#bikini06 h3').style.cssText = 'background-color: gold; color: black; transition: all 0.3s ease-in-out;'
+  }
+  sessionStorage.setItem('Store', wishListStore)
+}
+
+function shoppingid7() {
+  let wishlistShoppingItem1 = document.getElementById('bikini07')
+  if (wishlistShoppingItem1.classList.contains('active')) {
+    wishlistShoppingItem1.classList.remove('active')
+    wishListStore.shift(wishlistShoppingItem1.id)
+    document.querySelector('#bikini07 h3').style.cssText = ' '
+  } else {
+    wishlistShoppingItem1.classList.add('active')
+    wishListStore.push(wishlistShoppingItem1.id)
+    document.querySelector('#bikini07 h3').style.cssText = 'background-color: gold; color: black; transition: all 0.3s ease-in-out;'
+  }
+  sessionStorage.setItem('Store', wishListStore)
+}
+
+//For wishlist notifications
 let z = 103
 let x = 103
 for (let k = 0; k < wishListButton.length; k++) {
@@ -102,11 +207,15 @@ for (let k = 0; k < wishListButton.length; k++) {
       document.getElementsByClassName('gg-crown')[k + 1].style.cssText = 'background-color: transparent;'
     }
 })}
+
+//For moving ids from array to session storage
 for (let y = 0; y < wishListButton.length; y++) {
   wishListButton[y].addEventListener('click', function(){
     sessionStorage.setItem('Store', wishListStore)
   })
 }
+
+//For searchbar functionality and visibility
 let bar = document.getElementsByClassName('bikinibar')
 let triangle2 = document.getElementsByClassName('triangle2')
 function searchItems() {
@@ -139,7 +248,6 @@ document.getElementById('searchbar').addEventListener('click', function() {
     triangle2[0].style.display = 'flex'
   }
 })
-
 document.addEventListener('click', function(f) {
   if (document.getElementById('searchbar').contains(f.target) || bar[0].contains(f.target)){
     
@@ -148,6 +256,23 @@ document.addEventListener('click', function(f) {
     triangle2[0].style.display = 'none'
   }
 });
+
+//sizes color change
+const sizes = document.getElementsByClassName('sizeli')
+const sizelist = document.getElementsByClassName('sizelist')
+for (let s = 0; s < sizes.length; s++) {
+  sizelist[0].addEventListener('click', function(a) {
+    if (sizes[s].contains(a.target)) {
+      sizes[s].style.cssText = 'background-color: black; color: white;'
+      sizes[s].classList.add('cart')
+    } else {
+      sizes[s].style.cssText = ' '
+      sizes[s].classList.remove('cart')
+    }
+  })
+}
+
+//For bikini arrows functionality and visibility
 function show() {
   document.querySelector('.container').style.display = 'flex'
 }
@@ -160,4 +285,3 @@ document.querySelector('.box1').addEventListener('click', function() {
 document.querySelector('.box2').addEventListener('click', function() {
   document.querySelector('.grid').scrollBy({top:0, left:500, behavior:"smooth"})
 })
-
